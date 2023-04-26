5e45b946feb250ddf9d3691688775713896e39d7
Avel FERON PAPEL <avel.feron@atos.net>
2023-04-26 14:12:20 +0200

    premier commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {ProjectsListComponent} from "./components/projects-list/projects-list.component";
+import {ProjectDetailComponent} from "./components/project-detail/project-detail.component";
+import {ContactComponent} from "./components/contact/contact.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'projects', component: ProjectsListComponent},
+  {path:'projects/:id', component: ProjectDetailComponent},
+  {path:'contact', component: ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ProjectsListComponent } from './components/projects-list/projects-list.component';
+import { ProjectDetailComponent } from './components/project-detail/project-detail.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    ProjectsListComponent,
+    ProjectDetailComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
